903924fe24098ce34a5069004ad4cc811db0232f
Minh Nguyen <tmnguyen1403@gmail.com>
2020-09-28 19:46:40 -0500

    9/28/2020 Add MovieGrid

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Tom Riddle on 9/28/20.
+//  Copyright © 2020 Tommy. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+  @IBOutlet weak var posterView: UIImageView!
+}
