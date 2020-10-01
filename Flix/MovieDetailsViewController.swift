bcfa6a3f8f69e0b9b5d4b85745f5e53bd8bb37ae
Minh Nguyen <tmnguyen1403@gmail.com>
2020-10-01 00:59:19 -0500

    10/1/2020 Show trailer when click on poster
    1. Create segue from movieDetailsController to videoViewController
    2. Add gestureRecognizer to posterView. Implement function tapDetected
    3. Call performSegue in tapDetected to show trailer

--- Flix/MovieDetailsViewController.swift
+++ Flix/MovieDetailsViewController.swift
@@ -20,6 +20,10 @@ class MovieDetailsViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+      if let movieId = movie["id"]{
+        let test : String = "hello id: \(movieId)";
+        print(test);
+      }
       print(movie["title"]!);
       titleLabel.text = movie["title"] as? String;
       synopsisLabel.text = movie["overview"] as? String;
@@ -33,6 +37,11 @@ class MovieDetailsViewController: UIViewController {
       let backdropPath = movie["backdrop_path"] as! String;
       let backdropUrl = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath);
       backdropView.af.setImage(withURL: backdropUrl!);
+      
+      //setup tap event for poster
+      let oneTap = UITapGestureRecognizer(target: self, action: #selector(tapDetected));
+      posterView.isUserInteractionEnabled = true;
+      posterView.addGestureRecognizer(oneTap);
     }
     
 
@@ -45,5 +54,17 @@ class MovieDetailsViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+  @objc func tapDetected() {
+    print("Imageview Cliked");
+    performSegue(withIdentifier: "videoSegue", sender: nil);
+  }
+  
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if let movieId = movie["id"] {
+      let videoViewController = segue.destination as! VideoViewController;
+      videoViewController.movieId = movieId;
+    } else {
+      print("Movie doesn't have trailer");
+    }
+  }
 }
---
+++ Flix/VideoViewController.swift
@@ -0,0 +1,65 @@
+//
+//  VideoViewController.swift
+//  Flix
+//
+//  Created by Tom Riddle on 9/30/20.
+//  Copyright © 2020 Tommy. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class VideoViewController: UIViewController{
+
+  @IBOutlet weak var webView: WKWebView!
+  var movieId : Any?
+  var dataDictionary = [[String:Any]]()
+  var data = [[String:Any]]();
+  override func viewDidLoad() {
+        super.viewDidLoad()
+      
+    print("videoviewcontroller didload");
+        // Do any additional setup after loading the view.
+    //make request to get videoKey from API
+    if let id = movieId {
+        let apiKey : String = "a07e22bc18f5cb106bfe4cc1f83ad8ed";
+        let url = URL(string: "https://api.themoviedb.org/3/movie/\(id)/videos?api_key=\(apiKey)&language=en-US")!;
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+         // This will run when the network request returns
+         if let error = error {
+            print(error.localizedDescription)
+         } else if let data = data {
+          // TODO: Get the array of movies
+            let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+          // TODO: Store the movies in a property to use elsewhere
+            self.data = dataDictionary["results"] as! [[String:Any]];
+          // TODO: load the video
+          if let videoKey = self.data[0]["key"] {
+            let videoURL = URL(string: "https://www.youtube.com/watch?v=\(videoKey)&ab_channel=SuperAnon9876");
+            let videoRequest = URLRequest(url: videoURL!);
+            self.webView.load(videoRequest);
+          }
+          
+         }
+        }
+        task.resume();
+      }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+//  override func loadView() {
+//    webView.uiDelegate = self;
+//    
+//  }
+}
